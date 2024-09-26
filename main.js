8da11186aeb7868fe5fe3d56536f5a2a5700cc28
Raxhul <gv807962@gmail.com>
2024-09-26 11:28:52 +0530

    ..

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
-  res.send('This site is under raghul control')
+  res.send('Hello World!')
 });
 
 connnect();
